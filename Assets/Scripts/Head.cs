db3b3c3f9c8b8653289534b913cec51e52bcfc8d
ToineF <113095213+ToineF@users.noreply.github.com>
2024-11-21 18:35:54 +0100

    [PROGRAMMING] Movements rework with addForce ⛲

--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -1,20 +1,26 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Head : MonoBehaviour
 {
     public Vector2 Direction => _direction;
-    
-    [field:Header("Head Properties")]
-    [field:SerializeField] public Rigidbody Rigidbody { get; set; }
-    [field:SerializeField] public Rigidbody[] BodyParts { get; set; }
+
+    [field: Header("Head Properties")]
+    [field: SerializeField]
+    public Rigidbody Rigidbody { get; set; }
+
+    [field: SerializeField] public Rigidbody[] BodyParts { get; set; }
     [SerializeField] private float _speed;
-    [SerializeField, Range(0,1)] private float _turnLerp;
-    [SerializeField, Range(0,1)] private float _followLerp;
+    [SerializeField] private float _endBodySpeed;
+    [SerializeField, Range(0, 1)] private float _turnLerp;
+    [SerializeField, Range(0, 1)] private float _followLerp;
     [SerializeField] private float _targetDistance;
-    
-    [Header("Ground Detection")]
-    [SerializeField] private float _additionalGravity;
+    [SerializeField] private float forceIterations;
+
+    [Header("Ground Detection")] [SerializeField]
+    private float _additionalGravity;
+
     [SerializeField] private float _groundDetectionDistance;
     [SerializeField] private LayerMask _groundLayer;
 
@@ -22,8 +28,9 @@ public class Head : MonoBehaviour
     //public float forceTobODYPARTS = 1f;
     //public int forceiterations = 1;
 
-    [Header("Input Properties")]
-    [SerializeField] private KeyCode _rightKey;
+    [Header("Input Properties")] [SerializeField]
+    private KeyCode _rightKey;
+
     [SerializeField] private KeyCode _leftKey;
     [SerializeField] private KeyCode _upKey;
     [SerializeField] private KeyCode _downKey;
@@ -49,30 +56,59 @@ public class Head : MonoBehaviour
 
     private void FixedUpdate()
     {
-        AddForce();        
+        MoveSelf();
+        //MoveBodyParts();
+        ApproachBodyParts();
     }
 
-    private void AddForce()
+    private void MoveSelf()
     {
-        //Rigidbody.position += new Vector3(_direction.x, 0, _direction.y) * _speed;
         Rigidbody.AddForce(new Vector3(_direction.x, 0, _direction.y) * _speed, ForceMode);
         if (IsGrounded() == false) Rigidbody.AddForce(Vector3.down * _additionalGravity, ForceMode);
-        //for (int k = 0; k < forceiterations; k++)
+    }
+
+    private void MoveBodyParts()
+    {
+        for (int i = 0; i < BodyParts.Length; i++)
+        {
+            if (i == 0) continue;
+            var targetDirection = (BodyParts[i - 1].position - BodyParts[i].position).normalized;
+            //Vector3.Lerp(BodyParts[i].position, BodyParts[i - 1].position + distanceOffset, _followLerp);
+            targetDirection = Vector3.Lerp(_direction, targetDirection, _followLerp);
+            var speed = Mathf.Lerp(_speed, _endBodySpeed, i / (BodyParts.Length - 1));
+            var force = new Vector3(targetDirection.x, 0, targetDirection.y) * speed;
+            BodyParts[i].AddForce(force, ForceMode);
+            //BodyParts[i].AddForce(distanceOffset * forceToBodyParts);
+        }
+    }
+
+    private void ApproachBodyParts()
+    {
+        //for (int k = 0; k < forceIterations; k++)
         //{
             for (int i = 0; i < BodyParts.Length; i++)
             {
                 if (i == 0) continue;
-                var distanceOffset = (BodyParts[i].position - BodyParts[i - 1].position).normalized * _targetDistance;
-                BodyParts[i].position = Vector3.Lerp(BodyParts[i].position, BodyParts[i - 1].position + distanceOffset, _followLerp);
-                //BodyParts[i].AddForce(distanceOffset * forceTobODYPARTS);
+                var offset = (BodyParts[i - 1].position - BodyParts[i].position);
+                var targetDirection = offset.normalized;
+                //Vector3.Lerp(BodyParts[i].position, BodyParts[i - 1].position + distanceOffset, _followLerp);
+                //targetDirection = Vector3.Lerp(_direction, targetDirection, _followLerp);
+                //var speed = Mathf.Lerp(_speed, _endBodySpeed, i / (BodyParts.Length - 1));
+                if (offset.sqrMagnitude < _targetDistance) continue;
+                var force = new Vector3(targetDirection.x, 0, targetDirection.z) * offset.sqrMagnitude;
+                for (int k = 0; k < forceIterations; k++)
+                {
+                    BodyParts[i].AddForce(force, ForceMode);
+                }
+                //BodyParts[i].AddForce(distanceOffset * forceToBodyParts);
             }
         //}
-
     }
-    
+
     private bool IsGrounded()
     {
-        return Physics.RaycastNonAlloc(transform.position, Vector3.down, _groundHits, _groundDetectionDistance, _groundLayer) > 0;
+        return Physics.RaycastNonAlloc(transform.position, Vector3.down, _groundHits, _groundDetectionDistance,
+            _groundLayer) > 0;
     }
 
     private void OnDrawGizmosSelected()
