8995ba105841f1a23349f546b409dd87071d3844
ToineF <113095213+ToineF@users.noreply.github.com>
2025-01-13 14:01:30 +0100

    [PROGRAMMING] LD 06 UI

--- Assets/Scripts/Character/HeadVisualsFeedbacks.cs
+++ Assets/Scripts/Character/HeadVisualsFeedbacks.cs
@@ -5,6 +5,7 @@ public class HeadVisualsFeedbacks : MonoBehaviour
 {
     [SerializeField] private Head _head;
     [SerializeField] private ParticleSystem _glitterVFX;
+    [SerializeField] private GameObject _fakeHead;
     [SerializeField] private Transform _headHat;
     [SerializeField] private Transform _separatedParent;
     [SerializeField] private Transform _connectedParent;
@@ -18,6 +19,7 @@ public class HeadVisualsFeedbacks : MonoBehaviour
     private void OnConnect()
     {
         _glitterVFX.Play();
+        _fakeHead.SetActive(false);
         _headHat.SetParent(_connectedParent);
         _headHat.localPosition = Vector3.zero;
         _headHat.localEulerAngles = Vector3.zero;
@@ -26,6 +28,7 @@ public class HeadVisualsFeedbacks : MonoBehaviour
     private void OnSeparate()
     {
         _glitterVFX.Stop();
+        _fakeHead.SetActive(true);
         _headHat.SetParent(_separatedParent);
         _headHat.localPosition = Vector3.zero;
         _headHat.localEulerAngles = Vector3.zero;
