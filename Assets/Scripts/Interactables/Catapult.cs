e46fd900817f9f5d57de3a46e70960e3e816769e
ToineF <113095213+ToineF@users.noreply.github.com>
2025-01-28 17:26:51 +0100

    [PROGRAMMING] Catapult 👩‍🦲

---
+++ Assets/Scripts/Interactables/Catapult.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Catapult : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _catapultBody;
+    [SerializeField] private Vector3 _torque;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            _catapultBody.AddTorque(_torque, ForceMode.Impulse);
+        }
+    }
+}
