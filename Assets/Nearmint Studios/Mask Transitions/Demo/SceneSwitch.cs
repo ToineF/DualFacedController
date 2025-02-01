e82c3ceb8c1748d1f33952bd8b3702f19d79b093
ToineF <113095213+ToineF@users.noreply.github.com>
2025-02-01 17:26:26 +0100

    [PROGRAMMING] Mice Transition 🎅

---
+++ Assets/Nearmint Studios/Mask Transitions/Demo/SceneSwitch.cs
@@ -0,0 +1,43 @@
+﻿namespace MaskTransitions
+{
+    using UnityEngine;
+
+    public class SceneSwitch : MonoBehaviour
+    {
+        public string sceneToLoadName;
+        public float totalTransitionTime;
+        
+        public void SwitchScene()
+        {
+            TransitionManager.Instance.LoadLevel(sceneToLoadName);
+        }
+
+        public void PlayTransition()
+        {
+            TransitionManager.Instance.PlayTransition(totalTransitionTime);
+        }
+
+        void PlayStartOfTransition()
+        {
+            TransitionManager.Instance.PlayStartHalfTransition(totalTransitionTime / 2);
+        }
+        void PlayEndOfTransition()
+        {
+            TransitionManager.Instance.PlayEndHalfTransition(totalTransitionTime / 2);
+        }
+
+        /*#if UNITY_EDITOR
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                PlayStartOfTransition();
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                PlayEndOfTransition();
+            }
+        }
+        #endif*/
+    }
+}
