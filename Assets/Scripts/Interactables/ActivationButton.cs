d81e903f985ed59cd8bd30dd6c87059680c2f957
AntoineF00 <113095213+ToineF@users.noreply.github.com>
2025-02-12 18:18:25 +0100

    [PROGRAMMING] Syncronisation Buttons Group 👨‍🔬

--- Assets/Scripts/Interactables/ActivationButton.cs
+++ Assets/Scripts/Interactables/ActivationButton.cs
@@ -6,12 +6,35 @@ namespace Cattac.Interactables
 {
     public class ActivationButton : MonoBehaviour, IGrabbable
     {
-        [SerializeField] private UnityEvent OnPressed;
+        public bool IsActivated { get; private set; }
+        
+        [field:SerializeField] public UnityEvent OnPressed { get; private set; }
+
+        [Header("Timer")]
+        [SerializeField] private float _timer;
+        [field:SerializeField] public UnityEvent OnTimerEnd { get; private set; }
+
+        private float _internalTimer = -1f;
 
         public void OnGrab(CharacterHead characterHead)
         {
             OnPressed?.Invoke();
             characterHead.CurrentGrabbable = null;
+            IsActivated = true;
+            _internalTimer = _timer;
+        }
+
+        private void Update()
+        {
+            if (_internalTimer <= 0) return;
+            
+            _internalTimer -= Time.deltaTime;
+
+            if (_internalTimer <= 0)
+            {
+                OnTimerEnd?.Invoke();
+                IsActivated = false;
+            }
         }
 
         public void OnUngrab(CharacterHead characterHead)
---
+++ Assets/Scripts/Interactables/ActivationButtonGroup.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Cattac.Interactables
+{
+    public class ActivationButtonGroup : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _onActivated;
+        [SerializeField] private ActivationButton[] _buttons;
+
+        private void Start()
+        {
+            foreach (var button in _buttons)
+            {
+                button.OnPressed.AddListener(CheckActivation);
+            }
+        }
+
+        private void CheckActivation()
+        {
+            foreach (var button in _buttons)
+            {
+                if (button.IsActivated == false) return;
+            }
+            
+            foreach (var button in _buttons)
+            {
+                button.OnPressed.RemoveAllListeners();
+                button.OnTimerEnd.RemoveAllListeners();
+                Destroy(button);
+            }
+            
+            _onActivated?.Invoke();
+        }
+    }
+}
