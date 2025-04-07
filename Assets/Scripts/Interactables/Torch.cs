7d3caefffee50d3da099bc2686635e0713c887bc
ToineF <113095213+ToineF@users.noreply.github.com>
2025-04-07 08:55:42 +0200

    [PROGRAMMING] Torch Prefab 🙆‍♀️

--- Assets/Scripts/Interactables/Torch.cs
+++ Assets/Scripts/Interactables/Torch.cs
@@ -1,17 +1,23 @@
 using Cattac.Character.Ability;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Cattac.Interactables
 {
     public class Torch : BoxTrigger
     {
+        [Header("Torch")]
         [SerializeField] private GameObject _vfx;
+        [SerializeField] private UnityEvent _onLitEvent;
+        [SerializeField] private UnityEvent _onUnlitEvent;
         
         protected override void OnEnterTriggerInternal(Collider other)
         {
             if (other.TryGetComponent(out FireWater water) == false) return;
             
             _vfx.SetActive(water.IsFire);
+            if (water.IsFire) _onLitEvent?.Invoke();
+            else _onUnlitEvent?.Invoke();
         }
     }
 }
