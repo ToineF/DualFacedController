6d0962c80bd89530cb221f61ee7ddabcfc50b09b
Didine_Sama <mahiedine.boudrissa@gmail.com>
2025-05-17 23:17:32 +0200

    [ART] Level Art update

--- Assets/Scripts/Collectibles/LevelCollectiblesData.cs
+++ Assets/Scripts/Collectibles/LevelCollectiblesData.cs
@@ -1,32 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Cattac.Interactables;
-using NaughtyAttributes;
-using UnityEditor;
-using UnityEngine;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using Cattac.Interactables;
+//using NaughtyAttributes;
+//using UnityEditor;
+//using UnityEngine;
 
-namespace Cattac.Collectibles
-{
-    public class LevelCollectiblesData : MonoBehaviour
-    {
-        public Action<int> OnMouseGain;
+//namespace Cattac.Collectibles
+//{
+//    public class LevelCollectiblesData : MonoBehaviour
+//    {
+//        public Action<int> OnMouseGain;
         
-        [field:SerializeField] public List<Cage> Mice { get; private set; }
+//        [field:SerializeField] public List<Cage> Mice { get; private set; }
         
-        public void SaveMouse(Cage mouse)
-        {
-            var index = Mice.FindIndex(e => e == mouse);
-            if (index == -1) return;
+//        public void SaveMouse(Cage mouse)
+//        {
+//            var index = Mice.FindIndex(e => e == mouse);
+//            if (index == -1) return;
 
-            OnMouseGain?.Invoke(index);
-        }
+//            OnMouseGain?.Invoke(index);
+//        }
 
-        [Button("Find Mice in Scene")]
-        public void AssignMice()
-        {
-            Mice = GameObject.FindObjectsOfType<Cage>().Reverse().ToList();
-            EditorUtility.SetDirty(gameObject);
-        }
-    }
-}
+//        [Button("Find Mice in Scene")]
+//        public void AssignMice()
+//        {
+//            Mice = GameObject.FindObjectsOfType<Cage>().Reverse().ToList();
+//            EditorUtility.SetDirty(gameObject);
+//        }
+//    }
+//}
