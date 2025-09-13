9399f42a988458deaf7b3deab581df75d1b2998b
AntoineF00 <113095213+ToineF@users.noreply.github.com>
2025-09-13 20:57:14 +0200

    [PROGRAMMING] Menu Colorblindness Option 🏂

---
+++ Assets/Scripts/Menu/Options/ColorBlindOption.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using SOHNE.Accessibility.Colorblindness;
+
+public class ColorBlindButtons : MonoBehaviour
+{
+    [SerializeField] private Button[] _buttons;
+
+    private void Start()
+    {
+	for (int i=0; i<_buttons.Length; i++)
+	{
+	    int index = i;
+	    _buttons[i].onClick.AddListener(() => OnClick(index));
+	}
+    }
+
+    private void OnClick(int index)
+    {
+	Colorblindness.Instance.InitChange(index);
+    }
+}
