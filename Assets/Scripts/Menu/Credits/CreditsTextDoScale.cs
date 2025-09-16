716d5a381113f8c6b6a9a10d858de4f32bdfc1f7
Antoine Foucault <113095213+ToineF@users.noreply.github.com>
2025-09-16 19:26:55 +0200

    [PROGRAMMING] Credits Disparition + Parameters for the inspector 🚣‍♂️

--- Assets/Scripts/Menu/Credits/CreditsTextDoScale.cs
+++ Assets/Scripts/Menu/Credits/CreditsTextDoScale.cs
@@ -6,9 +6,20 @@ namespace Cattac.Character.Credits
 {
     public class CreditsTextDoScale : MonoBehaviour
     {
+        [SerializeField] private float _maxTime;
+        
+        [Header("Tweens")]
+        [SerializeField] private float _writeTimeBetweenChars = 0.15f;
+        [SerializeField] private float _writeApparitionDuration = 0.5f;
+        [SerializeField] private Ease _writeEase = Ease.OutBack;
+        [SerializeField] private float _eraseTimeBetweenChars = 0.04f;
+        [SerializeField] private float _eraseDisparitionDuration = 0.5f;
+        [SerializeField] private Ease _eraseEase = Ease.OutBack;
+        
         private void Start()
         {
             StartCoroutine(Spawn());
+            StartCoroutine(Erase());
         }
 
         private IEnumerator Spawn()
@@ -20,8 +31,18 @@ namespace Cattac.Character.Credits
 
             for (int i = 0; i < transform.childCount; i++)
             {
-                transform.GetChild(i).DOScale(Vector3.one, 0.5f).SetEase(Ease.OutBack);
-                yield return new WaitForSeconds(0.15f);
+                transform.GetChild(i).DOScale(Vector3.one, _writeApparitionDuration).SetEase(_writeEase);
+                yield return new WaitForSeconds(_writeTimeBetweenChars);
+            }
+        }
+        
+        private IEnumerator Erase()
+        {
+            yield return new WaitForSeconds(_maxTime);
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).DOScale(Vector3.zero, _eraseDisparitionDuration).SetEase(_eraseEase);
+                yield return new WaitForSeconds(_eraseTimeBetweenChars);
             }
         }
     }
