4605779af330736f0180c943d73e829bc3d9667f
Antoine Foucault <113095213+ToineF@users.noreply.github.com>
2025-10-02 09:42:12 +0200

    [PROGRAMMING] Separator Kinematic 2 Players Bug Fix 🤸‍♂️

--- Assets/Scripts/Interactables/Separator.cs
+++ Assets/Scripts/Interactables/Separator.cs
@@ -7,24 +7,42 @@ namespace Cattac.Interactables
     {
         [SerializeField] private bool _separate;
         [SerializeField] private bool _makeKinematic = false;
+        private CharacterHead _currentHead; // used only if (_makeKinematic)
 
         public void OnGrab(CharacterHead characterHead)
         {
             if (characterHead.IsSeparated == _separate) return;
+            if (_makeKinematic && _currentHead) return; // Ensure no two heads are attached at the same time if kinematic
 
             characterHead.CurrentGrabbable = null;
             characterHead.SetSeparation(_separate);
             OnGrabInternal(characterHead);
-	    if (_makeKinematic) characterHead.TogetherRigidbody.isKinematic = _separate;
+            if (_makeKinematic)
+            {
+                characterHead.TogetherRigidbody.isKinematic = _separate;
+                _currentHead = characterHead;
+                characterHead.OnConnect += Reconnect;
+            }
         }
-        
-        protected virtual void OnGrabInternal(CharacterHead characterHead) { }
+
+        private void Reconnect()
+        {
+            _currentHead.OnConnect -= Reconnect;
+            _currentHead = null;
+        }
+
+        protected virtual void OnGrabInternal(CharacterHead characterHead)
+        {
+        }
 
         public void OnUngrab(CharacterHead characterHead)
         {
             OnUngrabInternal(characterHead);
         }
-        protected virtual void OnUngrabInternal(CharacterHead characterHead) { }
+
+        protected virtual void OnUngrabInternal(CharacterHead characterHead)
+        {
+        }
 
         public void AddForce(Vector3 force)
         {
