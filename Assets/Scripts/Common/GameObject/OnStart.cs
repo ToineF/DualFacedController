2f3dbf88e52dd398940c017796de1eadfd1f9957
Antoine Foucault <113095213+ToineF@users.noreply.github.com>
2025-11-19 16:23:06 +0100

    [LEVEL DESIGN] End Level Celebration in Build Scene 💑

--- Assets/Scripts/Common/GameObject/OnStart.cs
+++ Assets/Scripts/Common/GameObject/OnStart.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+/// <summary>
+/// Do something when Start is played
+/// </summary>
 public class OnStart : MonoBehaviour
 {
     [SerializeField] private UnityEvent _onStart;
