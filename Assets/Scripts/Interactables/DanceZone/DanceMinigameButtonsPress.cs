90b9f7c7cf376fd3e0f03f867713480c12975d5a
ToineF <113095213+ToineF@users.noreply.github.com>
2026-02-06 00:13:10 +0100

    [PROGRAMMING] New Dance Minigame Start 🥵

--- Assets/Scripts/Interactables/DanceZone/DanceMinigameButtonsPress.cs
+++ Assets/Scripts/Interactables/DanceZone/DanceMinigameButtonsPress.cs
@@ -1,25 +1,23 @@
+using System.Linq;
 using AntoineFoucault.Utilities;
-using DG.Tweening;
+using FeedbacksEditor;
 using NaughtyAttributes;
 using UnityEngine;
 
 namespace Cattac.Interactables
 {
-    public class DanceMinigame : MonoBehaviour
+    public class DanceMinigameButtonsPress : MonoBehaviour
     {
         [SerializeField] private Dancefloor _dancefloor;
-        [SerializeField] private GameObject _cheesePrefab;
-        [SerializeField] private Animator[] _cheeseSources;
-        [SerializeField] private Transform _center;
-        [SerializeField] private Transform _cheeseParent;
-        [SerializeField, MinMaxSlider(0f,40f)] private Vector2 _centerOffset;
+        [SerializeField] private PressurePlate[] _pressurePlates;
+        [SerializeField] private GameObject[] _spotlights;
+        
         [SerializeField, MinMaxSlider(0.01f,10f)] private Vector2 _spawnIntervalStart;
         [SerializeField, MinMaxSlider(0.01f,10f)] private Vector2 _spawnIntervalEnd;
-        [SerializeField] private float _cheeseMoveTime;
-        [SerializeField] private Ease _cheeseMoveEase;
         [SerializeField] private float _totalMinigameTime;
         [SerializeField] private GameObject[] _minigameEndActivate;
         [SerializeField] private GameObject[] _minigameEndDeactivate;
+        [SerializeField] private GameEvent _highlightPressurePlatesEvent;
 
         private float _spawnTimer;
         private float _totalMinigameTimer;
@@ -33,6 +31,7 @@ namespace Cattac.Interactables
         private void OnPartyStart()
         {
             _canUpdate = true;
+            _spotlights.SetAllActive(true);
         }
 
         private void Update()
@@ -49,21 +48,21 @@ namespace Cattac.Interactables
             
             if (_spawnTimer < 0)
             {
-                SpawnCheese();
+                HighlightPressurePlates();
             }
         }
 
-        private void SpawnCheese()
+        private void HighlightPressurePlates()
         {
             var spawnInterval = Vector2.Lerp(_spawnIntervalStart, _spawnIntervalEnd,
                 _totalMinigameTimer / _totalMinigameTime);
             _spawnTimer = UnityEngine.Random.Range(spawnInterval.x, spawnInterval.y);
-            var source = _cheeseSources.GetRandomItem();
-            var cheese = Instantiate(_cheesePrefab, source.transform.position, source.transform.rotation);
-            var direction = _center.position - source.transform.position;
-            var centerOffset = UnityEngine.Random.Range(_centerOffset.x, _centerOffset.y);
-            source.Play("Throw");
-            cheese.transform.DOMove(_center.position - direction.normalized * centerOffset, _cheeseMoveTime).SetEase(_cheeseMoveEase).OnComplete(() => cheese.transform.SetParent(_cheeseParent, true));
+            
+            var pressurePlate = _pressurePlates.GetRandomItem();
+            var pressurePlate2 = _pressurePlates.GetRandomItemExcluding(_pressurePlates.ToList().IndexOf(pressurePlate));
+            _spotlights[0].transform.position = new Vector3(pressurePlate.transform.position.x, _spotlights[0].transform.position.y, pressurePlate.transform.position.z);
+            _spotlights[1].transform.position = new Vector3(pressurePlate2.transform.position.x, _spotlights[1].transform.position.y, pressurePlate2.transform.position.z);
+            GameEventsManager.PlayEvent(_highlightPressurePlatesEvent, gameObject);
         }
 
         private void MinigameEnd()
