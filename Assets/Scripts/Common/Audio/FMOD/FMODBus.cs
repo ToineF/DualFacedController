7273ec0a0125ac9e95fe481b0a9f385ebeca1831
Antoine Foucault <113095213+ToineF@users.noreply.github.com>
2026-03-05 18:45:42 +0100

    [AUDIO] FMOD Volume Menu 👹

--- Assets/Scripts/Common/Audio/FMOD/FMODBus.cs
+++ Assets/Scripts/Common/Audio/FMOD/FMODBus.cs
@@ -10,6 +10,9 @@ namespace FMOD
         [SerializeField] private string _name;
         [SerializeField] private Slider _slider;
 
+        private readonly float _minSliderValue = -20f;
+        private readonly float _maxSliderValue = 10f;
+        
         private void Awake()
         {
             _slider.onValueChanged.AddListener(UpdateBus);
@@ -18,17 +21,39 @@ namespace FMOD
         private void Start()
         {
             _bus = FMODUnity.RuntimeManager.GetBus($"bus:/{_name}");
+            dd();
+            //getbusyes();
         }
 
         private void UpdateBus(float busVolume)
         {
-            _bus.setVolume(DecibelToLinear(busVolume));
+            _bus.setVolume(busVolume);
         }
 
-        private float DecibelToLinear(float dB)
+        private void dd()
         {
-            float linear = Mathf.Pow(10.0f, dB / 20f);
-            return linear;
+            _bus.getChannelGroup(out var group);
+            GetDSPByName(_bus, "dsds", out DSP dsds);
+        }
+        
+        public void GetDSPByName(FMOD.Studio.Bus bus, string targetName, out FMOD.DSP dsp)
+        {
+            dsp = new FMOD.DSP();
+
+            bus.getChannelGroup(out FMOD.ChannelGroup cg);
+            cg.getNumDSPs(out int numDSPs);
+
+            for (int i = 0; i < numDSPs; ++i)
+            {
+                cg.getDSP(i, out FMOD.DSP tmpDsp);
+
+                tmpDsp.getInfo(out string dspName, out uint version, out int channels, out int configwidth, out int configheight);
+
+                if (dspName.Contains(targetName))
+                {
+                    dsp = tmpDsp;
+                }
+            }
         }
     }
 }
