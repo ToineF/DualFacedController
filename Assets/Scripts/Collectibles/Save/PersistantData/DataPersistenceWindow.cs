67e90546f8bcdbc5e8034388d0d7ac6a0673e285
Antoine Foucault <113095213+ToineF@users.noreply.github.com>
2026-03-27 14:28:19 +0100

    [PROGRAMMING] Save System Window 🐨

---
+++ Assets/Scripts/Collectibles/Save/PersistantData/DataPersistenceWindow.cs
@@ -0,0 +1,39 @@
+using Cattac.Collectibles.Save;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataPersistenceWindow : EditorWindow
+{
+    [MenuItem("Cattac/Save System")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(DataPersistenceWindow));
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Rebake save system"))
+        {
+            BakeData();
+        }
+    }
+    
+#if UNITY_EDITOR
+    /// <summary>
+    /// Editor-only method to bake all 'Index'es of 'DataPersistence's
+    /// </summary>
+    private void BakeData()
+    {
+        var dataList = FindObjectsByType<DataPersistence>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+        for (int i = 0; i < dataList.Length; i++)
+        {
+            var data = dataList[i];
+            data.Index = i;
+            EditorUtility.SetDirty(data);
+        }
+
+        Debug.Log($"Persistent data baked in {SceneManager.GetActiveScene().name}");
+    }
+#endif
+}
