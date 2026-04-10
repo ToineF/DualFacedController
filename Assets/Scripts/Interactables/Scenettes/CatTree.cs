6f9da6fb1c6a8a17c07c61af7ae94e218ab491d8
Antoine Foucault <113095213+ToineF@users.noreply.github.com>
2026-04-10 14:45:24 +0200

    [NARRATION] Charzan 🐩

---
+++ Assets/Scripts/Interactables/Scenettes/CatTree.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CatTree : MonoBehaviour
+{
+    [SerializeField] private GameObject _catsParent;
+    [SerializeField] private Animator _treeAnimator;
+    [SerializeField] private Rigidbody[] _cats;
+    [SerializeField] private float _randomForce;
+
+    public void Fall()
+    {
+        _treeAnimator.enabled = true;
+        _catsParent.SetActive(true);
+        
+        for (int i = 0; i < _cats.Length; i++)
+        {
+            _cats[i].isKinematic = false;
+            _cats[i].AddForce(Random.insideUnitSphere * _randomForce, ForceMode.Impulse);
+        }
+    }
+}
