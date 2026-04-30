8d23cac180f79c88d1b9186dcbc0788aa1ce4ea8
Antoine Foucault <113095213+ToineF@users.noreply.github.com>
2026-04-30 14:36:21 +0200

    [PROGRAMMING] CharaController Animator Parameters 🐉

--- Assets/Scripts/Character/HeadAnimator.cs
+++ Assets/Scripts/Character/HeadAnimator.cs
@@ -9,8 +9,8 @@ namespace Cattac.Character.Visuals
     /// </summary>
     public class HeadAnimator : MonoBehaviour
     {
-        public CharacterHead CharacterHead => _characterHead;
         [SerializeField] private CharacterHead _characterHead;
+        [SerializeField] private BodyPartGravity _bodyPartGravity;
         [SerializeField] private Transform _neighbourBodyPart;
         [SerializeField] private Vector3 _positionOffset;
 
@@ -21,6 +21,11 @@ namespace Cattac.Character.Visuals
         private Transform _parent;
         private Vector3 _startOffset;
         private Camera _camera;
+        
+        private const string _isWalkingParameter = "IsWalking";
+        private const string _isGroundedParameter = "IsGrounded";
+        private const string _moveSpeedParameter = "MoveSpeed";
+        private const string _isSqueakingParameter = "IsSqueaking";
 
         private void Start()
         {
@@ -41,8 +46,11 @@ namespace Cattac.Character.Visuals
         private void UpdateAnimation()
         {
             if (_characterHead == null) return;
-            _animator.SetBool("IsWalking", _characterHead.InputDirection.sqrMagnitude > 0.1f);
-            _animator.SetBool("IsGrabbing", _characterHead.IsGrabbing);
+            _animator.SetBool(_isWalkingParameter, _characterHead.InputDirection.sqrMagnitude > 0.1f);
+            _animator.SetBool(_isSqueakingParameter, _characterHead.IsGrabbing);
+            var moveSpeed = Mathf.Clamp01(new Vector3(_characterHead.CurrentRigidbody.linearVelocity.x,  0, _characterHead.CurrentRigidbody.linearVelocity.z).sqrMagnitude/100);
+            _animator.SetFloat(_moveSpeedParameter, moveSpeed < 0.01f ? 0 : moveSpeed);
+            _animator.SetBool(_isGroundedParameter, _bodyPartGravity.IsGrounded);
         }
 
         private void RotateDirection()
@@ -70,8 +78,8 @@ namespace Cattac.Character.Visuals
                 neighbourPosition.y = 0;
                 var position = transform.position;
                 position.y = 0;
-                Quaternion _lookRotation = Quaternion.LookRotation((neighbourPosition - position).normalized);
-                transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * _lookAtLerp);
+                Quaternion lookRotation = Quaternion.LookRotation((neighbourPosition - position).normalized);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * _lookAtLerp);
             }
         }
     }
--- Assets/Scripts/Interactables/NPC/StateMachine/States/Cat_Chase_Mouse.cs
+++ Assets/Scripts/Interactables/NPC/StateMachine/States/Cat_Chase_Mouse.cs
@@ -12,44 +12,44 @@ namespace Cattac.Interactables.NPC.States
     {
         public override void StartState(Cat_StateManager stateManager)
         {
-            stateManager.StartCoroutine(dd(stateManager));
+            //stateManager.StartCoroutine(dd(stateManager));
         }
 
-        private IEnumerator dd(Cat_StateManager stateManager)
-        {
-            var target = stateManager.Seeker.Target.gameObject;
-            var animators = GameObject.FindObjectsByType < HeadAnimator>(FindObjectsSortMode.InstanceID);
-            GameObject animator = null;
-            GameObject otherHead = null;
-            foreach (var VARIABLE in animators)
-            {
-                if (VARIABLE.CharacterHead.gameObject == target)
-                {
-                    animator =  VARIABLE.gameObject;
-                }
-                else
-                {
-                    otherHead = VARIABLE.gameObject;
-                }
-            }
-            var targetPos = target.transform.position;
-            targetPos.y = stateManager.transform.position.y;
-            Debug.Log(target.gameObject.name + "at position" + targetPos, target);
-            var mover = stateManager.Mover.gameObject.transform.GetChild(0);
-            var moverOriginalPos = mover.transform.localPosition;
-
-            animator?.SetActive(false);
-            target?.SetActive(false);
-            mover.transform.DOMove(targetPos, stateManager.Data.ChaseMouseDuration).SetEase(stateManager.Data.ChaseMouseEase);
-
-            yield return new WaitForSeconds(1f);
-            mover.transform.DOLocalMove(moverOriginalPos, stateManager.Data.ChaseMouseDuration).SetEase(stateManager.Data.ChaseMouseEase);
-            yield return new WaitForSeconds(1f);
-            target.transform.parent.position = otherHead.transform.position;
-            animator?.SetActive(true);
-            target?.SetActive(true);
-
-        }
+        // private IEnumerator dd(Cat_StateManager stateManager)
+        // {
+        //     var target = stateManager.Seeker.Target.gameObject;
+        //     var animators = GameObject.FindObjectsByType < HeadAnimator>(FindObjectsSortMode.InstanceID);
+        //     GameObject animator = null;
+        //     GameObject otherHead = null;
+        //     foreach (var VARIABLE in animators)
+        //     {
+        //         if (VARIABLE.CharacterHead.gameObject == target)
+        //         {
+        //             animator =  VARIABLE.gameObject;
+        //         }
+        //         else
+        //         {
+        //             otherHead = VARIABLE.gameObject;
+        //         }
+        //     }
+        //     var targetPos = target.transform.position;
+        //     targetPos.y = stateManager.transform.position.y;
+        //     Debug.Log(target.gameObject.name + "at position" + targetPos, target);
+        //     var mover = stateManager.Mover.gameObject.transform.GetChild(0);
+        //     var moverOriginalPos = mover.transform.localPosition;
+        //
+        //     animator?.SetActive(false);
+        //     target?.SetActive(false);
+        //     mover.transform.DOMove(targetPos, stateManager.Data.ChaseMouseDuration).SetEase(stateManager.Data.ChaseMouseEase);
+        //
+        //     yield return new WaitForSeconds(1f);
+        //     mover.transform.DOLocalMove(moverOriginalPos, stateManager.Data.ChaseMouseDuration).SetEase(stateManager.Data.ChaseMouseEase);
+        //     yield return new WaitForSeconds(1f);
+        //     target.transform.parent.position = otherHead.transform.position;
+        //     animator?.SetActive(true);
+        //     target?.SetActive(true);
+        //
+        // }
 
         /*public override void UpdateState(Cat_StateManager stateManager)
         {
