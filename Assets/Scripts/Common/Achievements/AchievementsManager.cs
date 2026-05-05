331934312acf7676bbe939a6401f68a41bcae97b
Antoine Foucault <113095213+ToineF@users.noreply.github.com>
2026-05-05 19:28:55 +0200

    [PROGRAMMING] Save System Cleaning (a bit) 🦀

--- Assets/Scripts/Common/Achievements/AchievementsManager.cs
+++ Assets/Scripts/Common/Achievements/AchievementsManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AntoineFoucault.Utilities.Achievements
@@ -5,12 +6,21 @@ namespace AntoineFoucault.Utilities.Achievements
     public class AchievementsManager : MonoBehaviour
     {
         public static System.Action<AchievementData> OnGetAchievement;
+        public static System.Action<AchievementData> OnGetAllAchievements;
         
         private static AchievementsManager _achievementsManager;
+
+        [SerializeField] private List<AchievementData> _allAchievements;
         
+        private Dictionary<AchievementData, bool> _completedAchievements = new();
+
         private void Awake()
         {
-            _achievementsManager = this;
+            _completedAchievements = new Dictionary<AchievementData, bool>();
+            foreach (var achievement in _allAchievements)
+            {
+                _completedAchievements.Add(achievement, false);
+            }
         }
 
         public static void GetAchievement(AchievementData achievement)
@@ -18,5 +28,8 @@ namespace AntoineFoucault.Utilities.Achievements
             Debug.Log("Get Achievement : " + achievement.Title);
             OnGetAchievement?.Invoke(achievement);
         }
+        
+        // TODO : How to save ? Use static ? Player Prefs ? JSON File ?
+        // Maybe JSON would be easier because my entire save system is already working with player prefs.
     }
 }
