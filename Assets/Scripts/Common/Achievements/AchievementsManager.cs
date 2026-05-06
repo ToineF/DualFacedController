a98316a535e111f294d5b82d130e65829d4fc78d
Antoine Foucault <113095213+ToineF@users.noreply.github.com>
2026-05-06 19:14:34 +0200

    [PROGRAMMING] Achievements Manager Start 🐚

--- Assets/Scripts/Common/Achievements/AchievementsManager.cs
+++ Assets/Scripts/Common/Achievements/AchievementsManager.cs
@@ -1,18 +1,28 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 
 namespace AntoineFoucault.Utilities.Achievements
 {
     public class AchievementsManager : MonoBehaviour
     {
+        
+        // Current problems left :
+        // Option to reset save system
+        // Save good lines (never the same two lines) but don't erase already saved lines (maybe only save the achievements that are "true")
+        
         public static System.Action<AchievementData> OnGetAchievement;
         public static System.Action<AchievementData> OnGetAllAchievements;
-        
+
         private static AchievementsManager _achievementsManager;
 
         [SerializeField] private List<AchievementData> _allAchievements;
-        
-        private Dictionary<AchievementData, bool> _completedAchievements = new();
+
+        private static Dictionary<AchievementData, bool> _completedAchievements = new();
+
+        private const string _savePath = "/achievements.txt";
+        private const char _separatorChar = '$';
 
         private void Awake()
         {
@@ -21,15 +31,55 @@ namespace AntoineFoucault.Utilities.Achievements
             {
                 _completedAchievements.Add(achievement, false);
             }
+
+            Load();
         }
 
         public static void GetAchievement(AchievementData achievement)
         {
             Debug.Log("Get Achievement : " + achievement.Title);
             OnGetAchievement?.Invoke(achievement);
+            Save();
         }
-        
-        // TODO : How to save ? Use static ? Player Prefs ? JSON File ?
-        // Maybe JSON would be easier because my entire save system is already working with player prefs.
+
+        private static void Save()
+        {
+            var path = Application.persistentDataPath + _savePath;
+            StringBuilder achievementsBuilder = new StringBuilder();
+
+            foreach (var achievement in _completedAchievements)
+            {
+                achievementsBuilder.AppendLine(achievement.Key.Title + _separatorChar + achievement.Value);
+            }
+
+            File.WriteAllText(path, achievementsBuilder.ToString());
+            Debug.Log("Saved achievements at " + path);
+        }
+
+        private void Load()
+        {
+            var path = Application.persistentDataPath + _savePath;
+            if (File.Exists(Application.persistentDataPath + _savePath) == false)
+            {
+                Debug.LogError("No file existing at " + path);
+                return;
+            }
+
+            Debug.Log("Load achievements at " + path);
+            var lines = File.ReadAllLines(Application.persistentDataPath + _savePath);
+
+            foreach (var line in lines)
+            {
+                var lineContents = line.Split(_separatorChar);
+                foreach (var achievement in _allAchievements)
+                {
+                    if (lineContents[0] == achievement.Title)
+                    {
+                        _completedAchievements[achievement] = lineContents[1].ToLower() == "true";
+                        Debug.Log("Got achievement " + achievement.Title +" with value " + _completedAchievements[achievement]);
+                    }
+                }
+            }
+        }
     }
 }
