88b05729cf62830153ac7ef2d2cc13cfce970bbd
ToineF <antoine.foucault777@gmail.com>
2026-05-25 00:18:59 +0200

    [PROGRAMMING] Respawn Button in Pause Menu 🥚

---
+++ Assets/Scripts/Interactables/Death/RespawnEvent.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using Cattac.Character;
+using FeedbacksEditor;
+using MaskTransitions;
+using UnityEngine;
+
+namespace Cattac.Interactables.Death
+{
+    public class RespawnEvent : MonoBehaviour
+    {
+        [Header("Respawn Timings")]
+        [SerializeField] private float _transitionTime = 1f;
+        [SerializeField] private float _teleportatingDelay = .4f;
+        [SerializeField] private float _startDelay = .4f;
+        [SerializeField] private GameEvent _respawnFeedback;
+
+        public void Respawn()
+        {
+            Vector3? position = CheckpointsManager.CurrentCheckpoint?.transform.position;
+            var head = MainGame.Instance.PlayerController.GetComponentInChildren<CharacterHead>();
+            if (head && position.HasValue) StartCoroutine(RespawnCoroutine(head, position.Value));
+        }
+        
+        private IEnumerator RespawnCoroutine(CharacterHead characterHead, Vector3 position)
+        {
+            GameEventsManager.PlayEvent(_respawnFeedback, characterHead.gameObject);
+
+            yield return new WaitForSeconds(_startDelay);
+        
+            TransitionManager.Instance.PlayTransition(_transitionTime);
+        
+            yield return new WaitForSeconds(_teleportatingDelay);
+
+            characterHead.CharacterManager.TeleportPlayer(position);
+        
+        }
+    }
+}
