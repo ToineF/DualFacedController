a3641e9a287e781b14c5a6f65904a7f85c7c84b9
ToineF <antoine.foucault777@gmail.com>
2026-06-27 20:16:34 +0200

    [NARRATION] Arena Animations Cleaning 🥼

--- Assets/Scripts/Common/Animation/PlayRandomAnimation.cs
+++ Assets/Scripts/Common/Animation/PlayRandomAnimation.cs
@@ -5,6 +5,15 @@ public class PlayRandomAnimation : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     [SerializeField] private AnimationClip[] _clips;
+    [SerializeField] private bool _playOnStart = false;
+
+    private void Start()
+    {
+        if (_playOnStart)
+        {
+            Play();
+        }
+    }
 
     public void Play()
     {
